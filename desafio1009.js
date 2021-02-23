6e290eb17703a2f532cf04529b7ecf4882ddcca8
Debora <deboraro.61@gmail.com>
2021-02-22 21:19:06 -0300

    resolvido em js

---
+++ desafio1009.js
@@ -0,0 +1,53 @@
+/*
+
+URI Online Judge | 1009
+Salário com Bônus
+Adaptado por Neilor Tonin, URI  Brasil
+
+Timelimit: 1
+Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês, com duas casas decimais.
+
+Entrada
+O arquivo de entrada contém um texto (primeiro nome do vendedor) e 2 valores de dupla precisão (double) com duas casas decimais, representando o salário fixo do vendedor e montante total das vendas efetuadas por este vendedor, respectivamente.
+
+Saída
+Imprima o total que o funcionário deverá receber, conforme exemplo fornecido.
+
+Exemplos de Entrada	Exemplos de Saída
+JOAO
+500.00
+1230.30
+
+TOTAL = R$ 684.54
+
+PEDRO
+700.00
+0.00
+
+TOTAL = R$ 700.00
+
+MANGOJATA
+1700.00
+1230.50
+
+TOTAL = R$ 1884.58
+
+
+
+ */
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+let nome = lines[0];
+let salario = parseFloat(lines[1]);
+let vendas = parseFloat(lines[2]);
+
+let total = (salario + (vendas*0.15)).toFixed(2); 
+console.log(`TOTAL = R$ ${total}`)
