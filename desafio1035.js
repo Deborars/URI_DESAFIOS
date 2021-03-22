3b4b3b053bb1e990c3ced6f039ee1d60d97a576b
Debora <deboraro.61@gmail.com>
2021-03-21 21:02:20 -0300

    funciona, mas não passou no uri

---
+++ desafio1035.js
@@ -0,0 +1,21 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+let A = parseInt(lines[0]);
+let B = parseInt(lines[1]);
+let C = parseInt(lines[2]);
+let D = parseInt(lines[3]);
+
+let X = C + D;
+let Y = A + B;
+
+if(B>C && D>A && X>Y && A%2===0 && C>0 && D>0)
+    {console.log("Valores aceitos");}
+else{
+    console.log("Valores nao aceitos");
+}
