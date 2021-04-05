b816db7fe5bfe5e9cff05cb7c30e845556c5b64e
Debora <deboraro.61@gmail.com>
2021-04-05 18:17:01 -0300

    praticando history API

--- origamid/historyAPI/script.js
+++ origamid/historyAPI/script.js
@@ -5,20 +5,28 @@ const links = document.querySelectorAll('a');
 
  function handleClick(event){
   event.preventDefault();
-  fetchPage(event.target.href);
+  fetchPage(event.target.href);//passando a url do link
+  window.history.pushState(null, null, event.target.href);
 }
 
 async function fetchPage(url){
   const pageResponse = await fetch(url);
   const pageText = await pageResponse.text();
   replaceContent(pageText);
+  
+  
 }
 
-function replaceContent(text){
-  const div= document.createElement('div');
-  div.innerHTML = text;
-  console.log(div);
+function replaceContent(newText){
+  
+  const newHtml= document.createElement('div');
+  newHtml.innerHTML = newText;
+  const oldContent = document.querySelector('.content');
+  const newContent = newHtml.querySelector('.content');
+  oldContent.innerHTML=newContent.innerHTML;
+  document.title = newHtml.querySelector('title').innerText;
 }
+
 
 links.forEach((item)=>{
   item.addEventListener('click', handleClick);
